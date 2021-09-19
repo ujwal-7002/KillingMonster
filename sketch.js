b21604f7221788832876a13d3a70a21cebd93e14
ujwal-7002 <78682746+ujwal-7002@users.noreply.github.com>
2021-09-19 12:20:07 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -19,7 +19,7 @@ function setup() {
   ground = new Ground(600, 600, 1200, 20);
 
   hero = new Hero(400,800,250);
-  rope = new Rope(hero.body, { x: 500, y: 50 });
+  rope = new Rope(hero.body, { x: 550, y: 50 });
   monster = new Monster(1100,550,300);
 
   box1 = new Box(600, 200, 70, 70);
